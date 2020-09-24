021eef96f3f7aace5692b42a949719972404855e
Jonathan Beliën <dev@jbelien.be>
2020-09-24 16:05:21 +0200

    Create buffer.ts

---
+++ script/src/buffer.ts
@@ -0,0 +1,29 @@
+"use strict";
+
+import es from "event-stream";
+import fs from "fs";
+import { Feature } from "geojson";
+import JSONStream from "jsonstream-next";
+import path from "path";
+import buffer from "@turf/buffer";
+
+const args = process.argv.slice(2);
+
+const source = args[0];
+const target = args[1];
+
+const directory = path.dirname(source);
+
+fs.createReadStream(source)
+  .pipe(JSONStream.parse("features.*"))
+  .pipe(
+    es.mapSync((feature: Feature) => buffer(feature, 20, { units: "meters" }))
+  )
+  .pipe(
+    JSONStream.stringify(
+      '{"type":"FeatureCollection","features":[\n',
+      ",\n",
+      "\n]}"
+    )
+  )
+  .pipe(fs.createWriteStream(`${directory}/${target}`));
