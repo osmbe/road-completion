08c7eae51ca19ec41b12e62a0a9a83597b640dbd
Jonathan Beliën <dev@jbelien.be>
2020-09-24 14:58:36 +0200

    🎉 Initial commit

---
+++ script/src/convert-tags.ts
@@ -0,0 +1,58 @@
+"use strict";
+
+import es from "event-stream";
+import fs from "fs";
+import { Feature } from "geojson";
+import JSONStream from "jsonstream-next";
+import path from "path";
+
+const args = process.argv.slice(2);
+
+const source = args[0];
+const target = args[1];
+
+const directory = path.dirname(source);
+
+const convert = JSON.parse(
+  fs.readFileSync(path.dirname(directory) + "/convert.json").toString()
+);
+const convertKeys = Object.keys(convert);
+
+fs.createReadStream(source)
+  .pipe(JSONStream.parse("features.*"))
+  .pipe(
+    es.mapSync((feature: Feature) => {
+      let properties: { [index: string]: any } = {};
+      let tags: { [index: string]: any } = {};
+
+      if (feature.properties !== null) {
+        Object.keys(feature.properties).forEach((key: string) => {
+          if (feature.properties !== null) {
+            const value = feature.properties[key];
+
+            properties[`original:${key}`] = value;
+
+            if (convertKeys.indexOf(key) !== -1) {
+              if (typeof convert[key][value] !== "undefined") {
+                tags = Object.assign(tags, convert[key][value]);
+              } else if (typeof convert[key]["*"] !== "undefined") {
+                tags = Object.assign(tags, convert[key]["*"]);
+              }
+            }
+          }
+        });
+      }
+
+      feature.properties = Object.assign(tags, properties);
+
+      return feature;
+    })
+  )
+  .pipe(
+    JSONStream.stringify(
+      '{"type":"FeatureCollection","features":[\n',
+      ",\n",
+      "\n]}"
+    )
+  )
+  .pipe(fs.createWriteStream(`${directory}/${target}`));
