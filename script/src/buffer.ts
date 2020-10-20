b514065f9699d9c6f97a20420620a8b2504edb3e
Jonathan Beliën <dev@jbelien.be>
2020-10-20 11:45:32 +0200

    🔨 Update buffer.ts
    Add radius option.

--- script/src/buffer.ts
+++ script/src/buffer.ts
@@ -1,22 +1,24 @@
 "use strict";
 
+import buffer from "@turf/buffer";
 import es from "event-stream";
 import fs from "fs";
 import { Feature } from "geojson";
 import JSONStream from "jsonstream-next";
+import minimist from "minimist";
 import path from "path";
-import buffer from "@turf/buffer";
 
 import fileExists from "./file-exists";
 
-const args = process.argv.slice(2);
+const options = minimist(process.argv.slice(2));
+const source = options._[0];
+const target = options._[1];
+const radius = options.r ?? options.radius;
 
-const source = args[0];
-const target = args[1];
+console.log(`Source: ${path.resolve(source)}`);
+console.log(`Radius: ${radius}m.`)
 
-console.log(`Source: ${path.resolve(args[0])}`);
-
-if (fileExists(args[0]) !== true) process.exit(1);
+if (fileExists(source) !== true) process.exit(1);
 
 const directory = path.dirname(source);
 
@@ -25,7 +27,7 @@ console.log(path.resolve(directory, target));
 fs.createReadStream(source)
   .pipe(JSONStream.parse("features.*"))
   .pipe(
-    es.mapSync((feature: Feature) => buffer(feature, 20, { units: "meters" }))
+    es.mapSync((feature: Feature) => buffer(feature, radius, { units: "meters" }))
   )
   .pipe(
     JSONStream.stringify(
