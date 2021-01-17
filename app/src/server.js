94794f99ec86730ebc2e62394ab36902e24a3b3f
Jonathan Beliën <dev@jbelien.be>
2021-01-17 17:54:06 +0100

    [app] Update server.js

--- app/src/server.js
+++ app/src/server.js
@@ -3,11 +3,13 @@ import polka from "polka";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, GITHUBIO } = process.env;
 const dev = NODE_ENV === "development";
+const githubio = GITHUBIO === 'true';
 
 polka() // You can also use Express
   .use(
+    githubio ? 'road-completion' : '/',
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
