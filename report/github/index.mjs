3cce4af0585dcaf7587cd764cde862bb853a664f
Jonathan Beliën <dev@jbelien.be>
2022-05-20 16:05:07 +0200

    Revert change

--- report/github/index.mjs
+++ report/github/index.mjs
@@ -5,15 +5,11 @@ import getCommits from "./commits.mjs";
 import { PATHS } from "./constants.mjs";
 
 Object.keys(PATHS).forEach(async (key) => {
-  try {
-    const data = await getCommits(PATHS[key]);
-    const json = JSON.stringify(data);
+  const data = await getCommits(PATHS[key]);
+  const json = JSON.stringify(data);
 
-    writeFile(`public/data/${key}.json`, json, (err) => {
-      if (err) throw err;
-      console.log(`✅ The file "${key}.json" has been saved!`);
-    });
-  } catch (err) {
-    console.error(`GitHub API request failed for "${key}": ${err}`);
-  }
+  writeFile(`public/data/${key}.json`, json, (err) => {
+    if (err) throw err;
+    console.log(`✅ The file "${key}.json" has been saved!`);
+  });
 });
