1040947825fe6fa4ed964ecdcd4eebdb86b29af0
Jonathan Beliën <dev@jbelien.be>
2022-07-03 14:14:21 +0200

    👷 Add workflow_dispatch to Belgian processes

--- report/github/index.mjs
+++ report/github/index.mjs
@@ -5,11 +5,15 @@ import getCommits from "./commits.mjs";
 import { PATHS } from "./constants.mjs";
 
 Object.keys(PATHS).forEach(async (key) => {
-  const data = await getCommits(PATHS[key]);
-  const json = JSON.stringify(data);
+  try {
+    const data = await getCommits(PATHS[key]);
+    const json = JSON.stringify(data);
 
-  writeFile(`public/data/${key}.json`, json, (err) => {
-    if (err) throw err;
-    console.log(`✅ The file "${key}.json" has been saved!`);
-  });
+    writeFile(`public/data/${key}.json`, json, (err) => {
+      if (err) throw err;
+      console.log(`✅ The file "${key}.json" has been saved!`);
+    });
+  } catch (err) {
+    console.error(`GitHub API request failed for "${key}": ${err}`);
+  }
 });
