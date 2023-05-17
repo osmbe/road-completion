b5206df7b6be37d35b4e4ceb1f2de5de2c546bef
Guillaume Rischard <github@stereo.lu>
2023-05-17 01:40:59 -0400

    Filter razed roads

--- data/luxembourg/filter.sql
+++ data/luxembourg/filter.sql
@@ -1,2 +1,17 @@
-SELECT * FROM "TRP_VC" WHERE
-  "NOM_RUE" <> '' AND ("TRP_CLAS_L" <> 'Chemin de fer' OR ("TRP_CLAS_L" <> 'Non codé' AND "TRP_CLAS_L" <> 'autre'))
+SELECT * FROM "TRP_VC"
+WHERE 
+    -- Exclude ways where "NOM_RUE" is empty
+    "NOM_RUE" <> '' 
+    -- Exclude ways where "TRP_CLAS_L" is 'Chemin de fer', 'Non codé', or 'autre'
+    -- These are railroads or little paths.
+    AND "TRP_CLAS_L" NOT IN ('Chemin de fer', 'Non codé', 'autre')
+    -- Exclude rows where "shape_len" is equal to the lengths specified.
+    -- These lengths have sub-atomic precision (10^-17 meters is smaller than the size of an atom!)
+    -- and can therefore be used as convenient UIDs. Use these to filter razed roads.
+    AND "shape_len" NOT IN (
+        830.166626069713971,
+        131.914105731793,
+        113.935351615667,
+        143.610383935929,
+        298.383651161161
+    )
